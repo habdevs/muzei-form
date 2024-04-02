50e03012491b0fba91fea76c0e2dc246ae72798e
habdevs <habdevs@yandex.ru>
2024-04-02 23:09:39 +0500

    feat: intermediate radio card

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+function selectRadioCard(cardNo) {
+  const allRadioCards = document.querySelectorAll(".radio-card");
+  allRadioCards.forEach((element, index) => {
+    element.classList.remove("selected");
+  });
+  
+  const selectedCard = document.querySelector(".radio-card-" + cardNo);
+  selectedCard.classList.add("selected");
+  
+  const selectedText = document.querySelector(".text-center-" + cardNo);
+  if (selectedText) {
+    selectedText.classList.add("selected");
+  }
+  console.log('CARD', selectedCard);
+}
