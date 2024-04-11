42b8d15e1213bd883d1769d14f7dbe9a9c9661d3
habdevs <habdevs@yandex.ru>
2024-04-12 00:55:57 +0500

    feat: added build

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,19 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import VitePluginHtml from 'vite-plugin-html'
+
 
 export default defineConfig({
   root: resolve(__dirname, 'src'),
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        carousel: resolve(__dirname, 'src/carousel.html')
+      }
+    },
     outDir: '../dist'
   },
   server: {
     port: 8080
   },
-  plugins: [
-    VitePluginHtml({
-      inject: {
-        data: {
-          title: 'My App'
-        }
-      }
-    })
-  ]
 })
